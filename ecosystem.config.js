f3db54c37c9a33bb265781e180560b310324789a
Fabricio <contact@fabricioguardia.com>
2023-10-02 06:23:48 -0500

    pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      key  : 'allocations.pem',
+      user : 'ubuntu',
+      host : '54.157.228.23',
+      ref  : 'origin/main',
+      repo : 'git@github.com:codecallogic/StaffAllocation.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
