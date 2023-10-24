b90c6a1ec8cfae1bf427684c741aa9f7c672a00e
Fabricio <contact@fabricioguardia.com>
2023-10-24 12:31:08 -0500

    second test site deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,24 @@
+// module.exports = {
+//   apps : [{
+//     script: 'npm start'
+//   }],
+
+//   deploy : {
+//     production : {
+//       key  : 'allocations.pem',
+//       user : 'ubuntu',
+//       host : '18.206.197.91',
+//       ref  : 'origin/main',
+//       repo : 'git@github.com:codecallogic/StaffAllocation.git',
+//       path : '/home/ubuntu',
+//       'pre-deploy-local': '',
+//       'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+//       'pre-setup': '',
+//       'ssh_options': 'ForwardAgent=yes'
+//     }
+//   }
+// };
+
 module.exports = {
   apps : [{
     script: 'npm start'
@@ -7,7 +28,7 @@ module.exports = {
     production : {
       key  : 'allocations.pem',
       user : 'ubuntu',
-      host : '18.206.197.91',
+      host : '34.229.44.181',
       ref  : 'origin/main',
       repo : 'git@github.com:codecallogic/StaffAllocation.git',
       path : '/home/ubuntu',
