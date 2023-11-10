6c0209e11e184b7d436ea3c24fb827e48e54b1c4
Fabricio <contact@fabricioguardia.com>
2023-11-09 19:18:25 -0500

    ecosystem update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,6 @@
-// module.exports = {
-//   apps : [{
-//     name: "client",
-//     script: 'npm start'
-//   }],
-
-//   deploy : {
-//     production : {
-//       key  : 'allocations.pem',
-//       user : 'ubuntu',
-//       host : '54.82.246.0',
-//       ref  : 'origin/main',
-//       repo : 'git@github.com:codecallogic/StaffAllocation.git',
-//       path : '/home/ubuntu/client',
-//       'pre-deploy-local': '',
-//       'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
-//       'pre-setup': '',
-//       'ssh_options': 'ForwardAgent=yes'
-//     }
-//   }
-// };
-
 module.exports = {
   apps : [{
-    name   : "client",
+    name: "client",
     script: 'npm start'
   }],
 
@@ -30,7 +8,7 @@ module.exports = {
     production : {
       key  : 'allocations.pem',
       user : 'ubuntu',
-      host : '34.227.152.167',
+      host : '54.82.246.0',
       ref  : 'origin/main',
       repo : 'git@github.com:codecallogic/StaffAllocation.git',
       path : '/home/ubuntu/client',
@@ -41,3 +19,25 @@ module.exports = {
     }
   }
 };
+
+// module.exports = {
+//   apps : [{
+//     name   : "client",
+//     script: 'npm start'
+//   }],
+
+//   deploy : {
+//     production : {
+//       key  : 'allocations.pem',
+//       user : 'ubuntu',
+//       host : '34.227.152.167',
+//       ref  : 'origin/main',
+//       repo : 'git@github.com:codecallogic/StaffAllocation.git',
+//       path : '/home/ubuntu/client',
+//       'pre-deploy-local': '',
+//       'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+//       'pre-setup': '',
+//       'ssh_options': 'ForwardAgent=yes'
+//     }
+//   }
+// };
