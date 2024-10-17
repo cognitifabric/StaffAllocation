1b1af4de9b55e81426538c1d6adcf0c8963d1de7
Fabricio <contact@fabricioguardia.com>
2024-10-17 15:12:05 -0700

    pm2 ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,38 +6,16 @@ module.exports = {
 
   deploy : {
     production : {
-      key  : 'allocations.pem',
+      key  : 'arthur.pem',
       user : 'ubuntu',
-      host : '54.82.246.0',
-      ref  : 'origin/main',
+      host : '54.90.108.58',
+      ref  : 'origin/master',
       repo : 'git@github.com:codecallogic/StaffAllocation.git',
       path : '/home/ubuntu/client',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install --legacy-peer-deps && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
   }
-};
-
-// module.exports = {
-//   apps : [{
-//     name   : "client",
-//     script: 'npm start'
-//   }],
-
-//   deploy : {
-//     production : {
-//       key  : 'allocations.pem',
-//       user : 'ubuntu',
-//       host : '34.227.152.167',
-//       ref  : 'origin/main',
-//       repo : 'git@github.com:codecallogic/StaffAllocation.git',
-//       path : '/home/ubuntu/client',
-//       'pre-deploy-local': '',
-//       'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
-//       'pre-setup': '',
-//       'ssh_options': 'ForwardAgent=yes'
-//     }
-//   }
-// };
+}
